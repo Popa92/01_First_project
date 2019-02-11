1255d7144f8e797de2267b6a3f7242be5c95e698
popa92 <Habarnuam01>
2019-02-11 23:24:10 +0200

    Homework 1 Java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,30 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello Wojuiurlddddd!");
+        //1.Write a Java program to print 'Hello' on screen and then print your name on a separate line
+        System.out.println("ex 1");
+        System.out.println("Hello Coco");
+        System.out.println("I'm, Ovi");
+        //2.Write a Java program to print ‘The addition of 7 and 8 is: ‘, and then the sum of the two numbers
+        byte adunare0ex1=7+8;
+        System.out.println("ex 2: 7+8= " + adunare0ex1);
+        //3.Write a Java program to divide two numbers and print on the screen.
+        double divide0ex2=9.0/2;
+        System.out.println("ex 3: 9/2= " + divide0ex2);
+
+        /*4.Write a Java program to print the result of the following operations:
+        a. -5 + 8 * 6
+        b. (55+9) % 9 
+        c. 20 + (-3*5) / 8 
+        d. 5 + 15 / 3 * 2 - 8 % 3 
+         */
+        int operatie0ex4a=-5+8*6;
+        System.out.println("ex 4a: -5+8*6= " + operatie0ex4a);
+        int operatie0ex4b=(55+9) % 9;
+        System.out.println("ex 4b: (55+9) % 9= " + operatie0ex4b);
+        double operatie0ex4c= 20.0 + (-3*5) / 8.0;
+        System.out.println("ex 4c: 20 + (-3*5) / 8 = " + operatie0ex4c);
+        int operatie0ex4d= 5 + 15 / 3 * 2 - 8 % 3;
+        System.out.println("ex 4d: 5 + 15 / 3 * 2 - 8 % 3 = " + operatie0ex4d);
     }
 }
