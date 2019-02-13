e6e74ee7798f4975b2152df7ac60e4a1e382d029
popa92 <Habarnuam01>
2019-02-13 20:44:33 +0200

    Homework 1 Java

---
+++ src/Fetita.java
@@ -0,0 +1,14 @@
+public class Fetita {
+    String nume;
+   int varsta;
+   float greutate;
+   String nationaltate;
+   float inaltime;
+
+   public void hranesteCainele(){
+       System.out.println("Fetita hraneste cainele");
+   }
+    public void plimbaCainele(){
+        System.out.println("Fetita plimba cainele");
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,61 @@
 public class Main {
 
     public static void main(String[] args) {
-        //1.Write a Java program to print 'Hello' on screen and then print your name on a separate line
-        System.out.println("ex 1");
-        System.out.println("Hello Coco");
-        System.out.println("I'm, Ovi");
-        //2.Write a Java program to print ‘The addition of 7 and 8 is: ‘, and then the sum of the two numbers
-        byte adunare0ex1=7+8;
-        System.out.println("ex 2: 7+8= " + adunare0ex1);
-        //3.Write a Java program to divide two numbers and print on the screen.
-        double divide0ex2=9.0/2;
-        System.out.println("ex 3: 9/2= " + divide0ex2);
+//
+        Fetita alina = new Fetita();
 
-        /*4.Write a Java program to print the result of the following operations:
-        a. -5 + 8 * 6
-        b. (55+9) % 9 
-        c. 20 + (-3*5) / 8 
-        d. 5 + 15 / 3 * 2 - 8 % 3 
-         */
-        int operatie0ex4a=-5+8*6;
-        System.out.println("ex 4a: -5+8*6= " + operatie0ex4a);
-        int operatie0ex4b=(55+9) % 9;
-        System.out.println("ex 4b: (55+9) % 9= " + operatie0ex4b);
-        double operatie0ex4c= 20.0 + (-3*5) / 8.0;
-        System.out.println("ex 4c: 20 + (-3*5) / 8 = " + operatie0ex4c);
-        int operatie0ex4d= 5 + 15 / 3 * 2 - 8 % 3;
-        System.out.println("ex 4d: 5 + 15 / 3 * 2 - 8 % 3 = " + operatie0ex4d);
+        alina.nume="Alina";
+        alina.varsta=13;
+        alina.nationaltate="romnana";
+
+        Fetita andreea = new Fetita();
+
+        andreea.nume="Andreea";
+        andreea.varsta=10;
+        andreea.nationaltate="romnana";
+
+        Fetita denisa = new Fetita();
+
+        denisa.nume="Alina";
+        denisa.varsta=13;
+        denisa.nationaltate="romnana";
+
+
+
+
+
+
+
+
+
+    }
+
+    public static double divide(double x, double y) {
+
+        double rezultatD = x / y;
+        return rezultatD;
+    }
+
+
+    public static int ex4a (int q, int w, int e) {
+
+        int rezultat4a = -q + w * e;
+        return rezultat4a;
+    }
+    public static int ex4b (int r, int t, int y) {
+
+        int rezultat4b = (r+t) % y;
+        return rezultat4b;
+    }
+    public static int ex4c (int u, int i, int o, int p) {
+
+        int rezultat4c = u + (-i*o) / p;
+        return rezultat4c;
+    }
+
+    public static int ex4d (int a, int s, int d, int f,int g, int h) {
+
+        int rezultat4c = a + s / d * f - g % h;
+        return rezultat4c;
     }
 }
