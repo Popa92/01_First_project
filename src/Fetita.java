68ebf54b0d0c376611b1fb9ea265058da5a44576
popa92 <Habarnuam01>
2019-03-12 20:17:25 +0200

    test

--- src/Fetita.java
+++ src/Fetita.java
@@ -1,5 +1,6 @@
 public class Fetita {
     String nume;
+    String anume;
     int varsta;
     float greutate;
     String nationaltate;
@@ -8,9 +9,9 @@ public class Fetita {
     public void setNume(String nume) {
         System.out.println("Fetita hraneste cainele");
         this.nume = nume;
-}
+    }
 
-    public String getNume (){
+    public String getNume() {
         return this.nume;
     }
 
